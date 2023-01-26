b2a98a3f6d520aa358502324af22a8e2fc676626
DzenanDemirovic <ddemirovicdzenan5@gmail.com>
2023-01-26 18:46:16 +0100

    Commit

---
+++ Zadatci.c
@@ -0,0 +1,125 @@
+/*#include <stdio.h>
+2.1 Zadatak
+
+ main() {
+	printf("Od danas programiramo u jeziku C.")
+	return 0;
+}
+*/
+/*2.2 
+b) RAzmak na vrhu i na dnu 
+main (){
+	printf("\nPozdrav svima!\n");
+	return 0;
+}
+
+c)
+main (){
+	printf("\n Pozzdrav\nsvima\n");
+	return 0;
+}
+2.3 Ako su deklarisane sledece promenljive
+int a, b;
+long int i, j;
+double x, y;
+char f;
+napisati funkciju koja ucitava vrednosti sa tastature za sledece promenljive:
+a) a, b, x
+b) i, f, j
+v) x, y, f
+
+
+a)
+main(){
+	int a,b ;
+	double x;
+	printf("Unesite a");
+	scanf("%d", &a);
+	printf(" Unesite b" );
+	scanf("%d",&b);
+	printf("Unesite x");
+	scanf("%lf",&x);
+	return 0 ;
+	
+	
+}
+
+b)
+main(){
+	long int i,j;
+	char f;
+	printf("Unesite i");
+	scanf("%ld",&i);
+	printf("Unesite j");
+	scanf(" %ld",&j);
+	printf("Unesite f");
+	scanf("%c",%f);
+	return 0;
+	
+	
+	
+}
+2.4. Napisati naredbu kojom se štampaju:
+a) promenljiva h u dekadnom sistemu;
+b) realne promenljive h i u;
+v) promeljiva h u dekadnom, oktalnom i heksadecimalnom sistemu;
+g) štampa vrednost promenljive h tipa char.
+
+
+a) 
+main(){
+	int x;
+	printf("%d",x);
+	return 0;
+	
+}
+b)
+main(){
+	float x;
+	printf("%f",x);
+	return 0;
+		
+}
+c)
+main(){
+	int x,x,x
+ printf("%d %o %x", x, x, x); 
+ return 0;
+	
+	
+}
+ d)
+ main(){
+ 	char x;
+ 	printf("%c",x)
+ 	
+ }
+//2.5. Sastaviti program kojim se ucitavaju i prikazuju jedan ceo i jedan realan broj. 
+main(){
+	float x; 
+	int y;
+	printf("Unesite  realan broj: ");
+	scanf("%f",&x);
+	printf("Unesite ceo broj: ");
+	scanf("%d",&y);
+	printf(" \n Ceo broj je %d\n",y);
+	printf("\n Realan broj je %f\n",x);
+		
+		return 0;
+}
+
+*/ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
